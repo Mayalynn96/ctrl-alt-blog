c2be96890ead755c5f9458af1e0956c4f00e63f8
Maya Lynn <mayalynncohen@gmail.com>
2023-02-08 23:28:31 -0800

    added own posts and comment section

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,19 +1,51 @@
-document.querySelector("#postForm").addEventListener("submit", event=>{
+document.querySelector("#postForm").addEventListener("submit", event => {
     event.preventDefault();
     const postObj = {
-        post:document.querySelector("#postInput").value
+        post: document.querySelector("#postInput").value
     }
     fetch("/api/posts", {
-        method:"POST",
-        body:JSON.stringify(postObj),
-        headers:{
-            "Content-Type":"application/json"
+        method: "POST",
+        body: JSON.stringify(postObj),
+        headers: {
+            "Content-Type": "application/json"
         }
-    }).then(res=>{
-        if(res.ok){
-           location.href="/profile"
+    }).then(res => {
+        if (res.ok) {
+            location.href = "/profile"
         } else {
             alert("trumpet sound")
         }
+    })
+})
+
+// let commentForm = document.createElement("form");
+// let commentInput = document.createElement("textarea");
+// let submitBtn = document.createElement("button")
+
+// commentForm.appendChild(commentInput);
+// commentForm.appendChild(submitBtn);
+// document.querySelector(".addCommentDiv").appendChild(commentForm)
+
+document.querySelectorAll(".commentBtn").forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        let commentForm = document.createElement("form");
+        commentForm.setAttribute("class", "commentForm")
+        let commentInput = document.createElement("textarea");
+        let submitBtn = document.createElement("button");
+        submitBtn.textContent = "Submit";
+        let exitBtn = document.createElement("button");
+        exitBtn.textContent = "Exit";
+
+        exitBtn.addEventListener("click", event =>{
+            event.preventDefault()
+            commentForm.remove()
+        })
+
+        commentForm.appendChild(commentInput);
+        commentForm.appendChild(submitBtn);
+        commentForm.appendChild(exitBtn);
+        btn.parentElement.append(commentForm)
+
     })
 })

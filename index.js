c7b261edd7de2369ff8fc31824a298ef3f6f2568
Maya Lynn <mayalynncohen@gmail.com>
2023-02-09 23:23:43 -0800

    fixing heroku

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 const { User,Post,Comment} = require('./models');
 
 const sess = {
-    secret: process.env.SESSION_SECRET,
+    secret: "secret secret",
     cookie: {
         maxAge:1000*60*60*2
     },
